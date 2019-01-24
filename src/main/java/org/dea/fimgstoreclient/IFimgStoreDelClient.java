615c3c2905e1e467c2d2f396e4a5323d261878bd
kahlep <philip.kahle@uibk.ac.at>
2019-01-24 19:22:09 +0100

    IFimgStoreDelClient: add deleteFiles method

--- src/main/java/org/dea/fimgstoreclient/IFimgStoreDelClient.java
+++ src/main/java/org/dea/fimgstoreclient/IFimgStoreDelClient.java
@@ -1,7 +1,9 @@
 package org.dea.fimgstoreclient;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface IFimgStoreDelClient {
 	public boolean deleteFile(final String fileKey, int nrOfRetries) throws IOException;
+	public void deleteFiles(List<String> fileKeysToDelete, int nrOfRetries);
 }
